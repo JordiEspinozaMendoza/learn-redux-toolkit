854a16708ad738736357caeef384c263f7fe6222
Jordi <jordi8101@gmail.com>
2023-01-06 19:05:11 -0800

    Added store and counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Counter } from "./features/counter/Counter";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
